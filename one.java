f3dd1c39c11182f03a63f0db4f38c3b0b4b3d824
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-05-12 17:07:27 -0300

    chore: start learning

---
+++ one.java
@@ -0,0 +1,11 @@
+class PrimeiroPrograma {
+  public static void main(String[] args){
+    System.out.println("oi mundo");
+    }
+}
+
+class SegundoPrograma {
+  // sem o main
+  // projeta um erro no console
+  int idade = 10;
+}
