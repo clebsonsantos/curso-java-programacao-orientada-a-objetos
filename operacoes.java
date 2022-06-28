49e02b4439a7b8ad71685e5e803199fc74e8c695
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-06-28 12:34:24 -0300

    chore: operacoes basicas matematica

---
+++ operacoes.java
@@ -0,0 +1,15 @@
+class OperacoesMatematicas {
+  public static void main(String[] args){
+    int adicao = 7 + 5;
+    int subtracao = 7 - 5;
+    double multiplicar = 7 * 2.5;
+    int module = 5%2;
+
+    System.out.println(adicao);
+    System.out.println(subtracao);
+    System.out.println(multiplicar);
+    System.out.println(module);
+
+  }
+
+}
