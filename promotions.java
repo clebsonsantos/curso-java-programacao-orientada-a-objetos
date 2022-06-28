a8786f2b7f10df87d73d3ac932151a13e620e4cd
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-06-28 12:39:42 -0300

    chore: caching em operacoes double

---
+++ promotions.java
@@ -0,0 +1,7 @@
+public class promotions {
+  public static void main(String[] args){
+    double salario = 500;
+    int numero = (int) salario; // assume apenas o inteiro da var salario, apos o ponto flutuante é perdido
+    System.out.println(numero);
+  }
+}
