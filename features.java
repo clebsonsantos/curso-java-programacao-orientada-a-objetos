71a78094be2d0c5730633e3e2e2976d2d297caf6
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-07-05 12:59:40 -0300

    laços de repetição for, while, do while

---
+++ features.java
@@ -0,0 +1,20 @@
+class LacosRepeticao {
+  public static void main(String[] args){
+    int i;
+    for(i = 0; i < 10; i++){
+      System.out.println("Laço encerrou ");
+    }
+    int j = 0;
+    while (j < 10) {
+      System.out.println("Laço encerrou aqui também");
+      j++;
+      
+    }
+
+    int k = 0;
+    do{
+      System.out.println(k + " ao quadrado é " + (k*k));
+      k++;
+    }while(k < 100);
+  }
+}
