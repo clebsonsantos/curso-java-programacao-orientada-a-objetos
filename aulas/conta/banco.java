4022b0a66efa7324f7182689d2c2ceb5936eb3aa
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-09-19 16:54:08 -0300

    tratando retorno da função

--- aulas/conta/banco.java
+++ aulas/conta/banco.java
@@ -5,8 +5,13 @@ class Conta {
   double saldo;
   String titular;
 
-  void sacar(double valor){
-    saldo -= valor;
+  boolean sacar(double valor){
+    if (saldo >= valor) {
+      saldo -= valor;
+      return true;
+    } else {
+      return false;
+    }
   }
   void depositar(double valor){
     saldo += valor;
@@ -20,13 +25,19 @@ class Conta {
 }
 
 class Banco {
+
   public static void main(String[] args){
     Conta conta = new Conta();
     conta.codigo = 251;
     conta.saldo = 500;
     conta.titular = "Clebson";
-
     conta.sacar(200.05);
+
+    if(conta.sacar(600.05)) {
+      System.out.println("Operaçao realizada com sucesso!");
+    } else {
+      System.out.println("Ocorreu um erro durante a operação!");
+    }
     System.out.println(conta.saldo);
 
     conta.depositar(700.05);
