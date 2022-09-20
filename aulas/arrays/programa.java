411aae5cd587e1e8bada3f061e2e2b0a6e39a779
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-09-20 15:27:13 -0300

    iniciando arrays

---
+++ aulas/arrays/programa.java
@@ -0,0 +1,22 @@
+package aulas.arrays;
+
+
+class Programa {
+  public static void main(String[] args) {
+    int notas[] = new int[8];
+    notas[0] = 10;
+    notas[1] = 10;
+    notas[2] = 10;
+    notas[3] = 10;
+    notas[4] = 10;
+    notas[5] = 10;
+    notas[6] = 10;
+    notas[7] = 10;
+
+
+    for(int i = 0; i < 8; i++) {
+      System.out.println(notas[i]);
+    }
+  }
+
+}
