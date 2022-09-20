35bc25d56bb7f466009035efd70eb776a4c0a9df
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-09-20 15:16:58 -0300

    associação entre classes

--- aulas/conta/banco.java
+++ aulas/conta/banco.java
@@ -3,7 +3,7 @@ package aulas.conta;
 class Conta {
   int codigo;
   double saldo;
-  String titular;
+  Cliente titular;
 
   boolean sacar(double valor){
     if (saldo >= valor) {
@@ -37,14 +37,23 @@ class Funcionario {
   boolean estaNaEmpresa;
 }
 
+class Cliente {
+  String nome;
+  String cpf;
+}
+
+
 class Banco {
 
   public static void main(String[] args){
+    Cliente cliente1 = new Cliente();
+    Cliente cliente2 = new Cliente();
+    cliente1.nome = "Clebson";
+    cliente1.cpf = "123345";
     Conta conta = new Conta();
     conta.codigo = 251;
     conta.saldo = 500;
-    conta.titular = "Clebson";
-    conta.sacar(200.05);
+    conta.titular = cliente1;
 
     if(conta.sacar(600.05)) {
       System.out.println("Saque realizada com sucesso!");
@@ -59,7 +68,10 @@ class Banco {
     Conta conta2 = new Conta();
     conta2.codigo = 251;
     conta2.saldo = 8000;
-    conta2.titular = "Fábio";
+
+    cliente2.nome = "Fábio";
+    cliente2.cpf = "123415";
+    conta2.titular = cliente2;
 
     if (conta.transferir(conta2, 800)) {
       System.out.println("Transferencia realizada com sucesso");
